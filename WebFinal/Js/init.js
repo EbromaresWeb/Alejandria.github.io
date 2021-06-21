6870dffb235e3ca642c3b76064287b50a97909c2
EbromaresWeb <85920356+EbromaresWeb@users.noreply.github.com>
2021-06-20 20:44:00 -0500

    Update init.js

--- WebFinal/Js/init.js
+++ WebFinal/Js/init.js
@@ -1,6 +1,6 @@
  // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
+  var firebase.initializeApp ( {
     apiKey: "AIzaSyCEMq58Kzqt91oumC3IKp1Gm4ngZXnN0ao",
     authDomain: "web3cv50ebro.firebaseapp.com",
     projectId: "web3cv50ebro",
@@ -8,7 +8,7 @@
     messagingSenderId: "122777220340",
     appId: "1:122777220340:web:1b627d1cd3c4de69de9aa1",
     measurementId: "G-468FJR4793"
-  };
+  });
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
