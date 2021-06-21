564ffc7f7078811f96537bb0ba917521efd0bac2
EbromaresWeb <85920356+EbromaresWeb@users.noreply.github.com>
2021-06-20 20:07:00 -0500

    Create init.js

---
+++ WebFinal/Js/init.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCEMq58Kzqt91oumC3IKp1Gm4ngZXnN0ao",
+    authDomain: "web3cv50ebro.firebaseapp.com",
+    projectId: "web3cv50ebro",
+    storageBucket: "web3cv50ebro.appspot.com",
+    messagingSenderId: "122777220340",
+    appId: "1:122777220340:web:1b627d1cd3c4de69de9aa1",
+    measurementId: "G-468FJR4793"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
